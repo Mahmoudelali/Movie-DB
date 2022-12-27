a1b660550ac0e5f88fcad17d5f83474f77d66414
Mahmoud <mahmud.elali98@gmail.com>
2022-12-27 13:26:30 +0200

    Step 3

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const app = require('express')();
 const PORT = 3000;
+const time = new Date();
 
 app.listen(PORT, () => {
 	console.log(`ok`);
 });
 
-
+app.get('/test', (req, res) => {
+	// this function runs when the route is requested
+	res.status(200).send({
+		status: 200,
+		message: `${time.getHours()}:${time.getMinutes()}`,
+	});
+});
+// today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// http://localhost:3000/test
