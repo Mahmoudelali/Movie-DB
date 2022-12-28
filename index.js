736d8a4c1c2e4693d751d21b88a4a0cb278b75ab
Mahmoud <mahmud.elali98@gmail.com>
2022-12-28 22:50:35 +0200

    step 4

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 const app = require('express')();
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 const time = new Date();
 
-app.listen(PORT, () => {
-	console.log(`ok`);
+// Step 2
+app.get('/', (req, res) => {
+	res.send('ok');
 });
 
+// Step 3
 app.get('/test', (req, res) => {
-	// this function runs when the route is requested
-	res.status(200).send({
-		status: 200,
-		message: `${time.getHours()}:${time.getMinutes()}`,
+	res.send({ status: 200, message: 'ok' });
+});
+
+app.get('/time', (req, res) => {
+	res.send(`${time.getHours()}:${time.getMinutes()}`);
+});
+
+// Step 4
+app.get('/hello/:id', (req, res) => {
+	if (req.params.id) {
+		res.send({ status: 200, message: `Hello ${req.params.id}` });
+	}
+});
+
+app.get('/search', (req, res) => {
+	if (req.query) {
+		res.send({ status: 200, message: 'ok', data: req.query.s });
+	}
+	res.send({
+		status: 500,
+		error: true,
+		message: 'you have to provide a search',
 	});
 });
+
+app.listen(port, () => {
+	console.log(`listening on PORT : ${port}`);
+});
+
 // today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 // http://localhost:3000/test
